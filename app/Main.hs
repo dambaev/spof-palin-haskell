77d8635f5044dcb7f372455d99a51cb8022a9674
dambaev <dambaev@dambaev-host>
2016-05-13 17:32:07 +0800

    minor fixes

--- app/Main.hs
+++ app/Main.hs
@@ -18,12 +18,16 @@ import Prelude  hiding
     , putStrLn
     )
 
+import Data.ByteString.Builder as BB
+    ( hPutBuilder
+    )
 import Data.ByteString as BS
     ( getLine
     , append
     )
 import Data.ByteString.Char8 as C8
     ( readInt
+    , readInteger
     , putStrLn
     , length
     , take
@@ -33,13 +37,14 @@ import Data.ByteString.Char8 as C8
     , index
     , reverse
     , null
+    , empty
     )
 
 import Data.Maybe
 
 main :: IO ()
 main = do
-    mcount <- getLine >>= return . readInt
+    mcount <- getLine >>= return . C8.readInt 
     case mcount of
         Nothing -> return ()
         Just (!count,_ ) -> readLines count 
@@ -57,9 +62,9 @@ readLines !count = do
 
 getNextPalyndrome:: C8.ByteString-> C8.ByteString
 getNextPalyndrome line = 
-    let !line_len = C8.length line
-        half = line_len `div` 2
-        left = take half line
+    let !line_len =  fromIntegral $! C8.length line
+        half = fromIntegral $! line_len `div` 2
+        left = C8.take half line
         rleft = C8.reverse left
         left_len = C8.length left
         center_len = line_len `mod` 2
@@ -90,7 +95,7 @@ getNextPalyndrome line =
                 _ -> isMoreRec (currid+1)
     in
     case line_len of
-        _ | line_len < 2 || line == "10" -> "11"
+        _ | line_len < 2 || (line_len == 2 && line == "10") -> "11"
         _ | isRLeftMoreRight -> left `BS.append` center `BS.append` rleft
         _ -> next_left `BS.append` next_center `BS.append` (C8.reverse next_left)
 
@@ -103,7 +108,6 @@ incString string =
         inc' !string !id !acc | id == 0 = C8.pack ('1':acc)
         inc' !string !id !acc | C8.index string (id-1) == '9' = inc' string (id-1) ('0':acc)
         inc' !string !id !acc = (C8.take (id-1) string) `BS.append` (C8.pack ((incChar $! C8.index string (id-1)):acc))
-
    in  inc' string src_len []
 
 
